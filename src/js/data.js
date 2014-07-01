1947182c1572b94e6b50c5bc13daaf5b77a8192a
vasanthps <vasanthps@live.com>
2014-07-01 20:29:31 +0530

    incorporated autocomplete for text inputs

--- src/js/data.js
+++ src/js/data.js
@@ -5,23 +5,23 @@ function newLog() {
 
 newLog.prototype = {
         
-        date: "", 
-        project: "", 
-        task: "", 
-        skill: "", 
-        phase: "", 
-        place: "", 
-        comment: "", 
-        billable: true, 
-        duration: false, 
-        timeLog: 0, 
-        startTime: 0,
-        timerRunning: false,
-        totalDuration: 0,
-        parseTime: function(time) {
-            //Parse the user entered duration time
-        },
-        calculateDuration: function(start_time, end_time) {
-            //timer method
-        }
-    };
+    date: "", 
+    project: "", 
+    task: "", 
+    skill: "", 
+    phase: "", 
+    place: "", 
+    comment: "", 
+    billable: true, 
+    duration: false, 
+    startTime: 0,
+    timerRunning: false,
+    totalDuration: 0,
+    parseTime: function(time) {
+        //Parse the user entered duration time
+    },
+    calculateDuration: function(start_time, end_time) {
+        //timer method
+    }
+};
+    
--- src/js/home.js
+++ src/js/home.js
@@ -1,14 +1,23 @@
 var mainData = {};
 
-var app = angular.module('logApp', []);
+var app = angular.module('logApp', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
 
 app.controller('timeLogController', function ($scope, $timeout) {
     'use strict';
     mainData.logData = new newLog();
     mainData.digiTimer = new timer();
+    mainData.searchList = {};
+    mainData.searchList.projects = [ "TI-log", "TI-AIP", "ASDF" ];
+    mainData.searchList.tasks = [ "Complete html creation", "update the code to run async", "do documentation" ];
+    mainData.searchList.skills = [ "Labview", "c#", "javascript", "ms excel" ];
+    mainData.searchList.phases = [ "Development", "Design", "Testing", "Support" ];
+    mainData.searchList.places = [ "Bangalore", "Coimbatore", "TI India", "US - Dallas" ];
+    
     $scope.newLog = mainData.logData;
     
     $scope.currentTime =mainData.digiTimer.getDigitalTime();
+    
+    $scope.searchList = mainData.searchList;
     
     $scope.switchTimer = function() {
         mainData.logData.duration = !mainData.logData.duration;
